b6a0f7f8b92f74e19cde045ed5754c3143561e84
yukari-design-python <hatitdje@gmail.com>
2026-04-26 15:47:21 +0500

    Update my bot.py

--- my bot.py
+++ my bot.py
@@ -18,12 +18,15 @@ async def start_handler(message: types.Message):
 @dp.message(F.text.contains("instagram.com"))
 async def insta_handler(message: types.Message):
     wait = await message.answer("🔍 Qidirilmoqda, kuting...")
-    ydl_opts = {
+ydl_opts = {
         'quiet': True,
         'no_warnings': True,
         'extract_flat': True,
         'http_headers': {
-            'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/120.0.0.0 Safari/537.36',
+            'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/123.0.0.0 Safari/537.36',
+            'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,*/*;q=0.8',
+            'Accept-Language': 'en-US,en;q=0.5',
+            'Connection': 'keep-alive',
         },
     }
     try:
