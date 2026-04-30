1cd7e93b06d8a2ea059964d27ce724670ac4121a
yukari-design-python <hatitdje@gmail.com>
2026-04-30 12:10:52 +0500

    Update my bot.py

--- my bot.py
+++ my bot.py
@@ -48,14 +48,9 @@ try:
             
         builder.adjust(5)
         await wait.edit_text(text, reply_markup=builder.as_markup())
-
-    except Exception as e:
+except Exception as e:
         print(f"Xato: {e}")
         await wait.edit_text(f"🚀 Xatolik yuz berdi: {e}")
-    except Exception as e:
-        print(f"Xato: {e}")
-        await wait.edit_text(f"🚀 Xatolik yuz berdi: {e}")
-
 @dp.callback_query(F.data.startswith("final_dl:"))
 async def process_final_dl(call: types.CallbackQuery):
     idx = call.data.split(":")[1]
