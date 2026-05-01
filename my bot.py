e2f5c0d86153f100b8607db5c2fb7b1fd5a3ae62
yukari-design-python <hatitdje@gmail.com>
2026-05-01 13:51:03 +0500

    Update my bot.py

--- my bot.py
+++ my bot.py
@@ -5,7 +5,7 @@ from aiogram.utils.keyboard import InlineKeyboardBuilder
 import yt_dlp
 
 logging.basicConfig(level=logging.INFO)
-TOKEN = os.getenv("8479111656:AAGLuoapeIpQdSOp79xu652Fy2W4AacJlf0")
+TOKEN = os.getenv("8479111656:AAHZ306sKkuA3BuLELinEaZVun7iYQUBHZ8")
 bot = Bot(token=TOKEN)
 dp = Dispatcher()
 
